b55fca1670791cafa79bbd564fbc44ae4da53b73
INDULEKHA P S <144669727+indulekhaps@users.noreply.github.com>
2024-04-22 16:32:23 +0530

    Add files via upload

--- 34_REVERSE.sh
+++ 34_REVERSE.sh
@@ -4,9 +4,12 @@ echo "Enter strings separated by space:"
 read -a strings
 for ((i=0; i<${#strings[@]}; i++)); do
     rev=$(echo ${strings[i]} | rev)
-    reversed_strings[$i]=$rev
+  
+    echo "$rev"
 done
+echo
+echo
 echo "Reversed list of strings:"
-for string in "${reversed_strings[@]}"; do
-    echo "$(echo $string | rev)"
+for string in "${strings[@]}"; do
+    echo "$(echo $string | strings)"
 done
